7c0d90385e079776d1b57e8507b2cce6b3931f8a
Minjik06 <minjik525@deltalearns.ca>
2023-03-03 11:35:46 -0800

    random

---
+++ task1.py
@@ -0,0 +1,25 @@
+"""Number Guessing Game
+Have the computer generate a random number from 1 to 100.  The players will try to guess the number, and the computer will tell them if they are too high or too low.  Play continues until they guess correctly at which point the computer tells them how many guesses it took.
+(2 points) """
+import random
+import this
+a=str(input("Enter a user name: "))
+print("Start the game")
+ran= random.randint(1,101)
+print("random numer is chosen, enter the number from 1 to 100")
+b=int(input("1 guess: "))
+if b==ran:
+    print("congratulations, you got the answer")
+else:
+    print("wrong answer")
+    if b<ran:
+        print("random numer is bigger than your answer")
+    else:
+        print("random number is smaller than your answer")
+while ran!=int(input(" guess again: ")):
+    k=int(input(" guess again: "))
+    if k<ran:
+        print("try again, random number is bigger than your answer")
+    elif k>ran:
+        print("try again, random nuber is smaller than your answer")
+print("finally you got the answer")
