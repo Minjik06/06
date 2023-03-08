9ed94fbf6d389d54c1e8dd2c735a33107490962e
Minjik06 <minjik525@deltalearns.ca>
2023-03-08 11:27:01 -0800

    random

--- task3.py
+++ task3.py
@@ -1,36 +1,43 @@
 import random
-a= str(input("rock vs paper vs scissors: "))
 p=0
 ran= random.randint(0,2)  # 0 rock, 1 paper, 2 scissor
-while p==5:
+while p<5:
+    ran= random.randint(0,2)
+    a= str(input("rock vs paper vs scissors: "))
     if ran == 0:
         if a=="rock":
             print("tie")
+            p=p+1
         elif a=="paper":
             print("you win")
-            p=p+1
+            p=p+2
         else:
             print("you lose")
-        print("point: ",p)
+            p=p-0.5
     elif ran == 1:
         if a=="rock":
             print("you lose")
+            p=p-0.5
         elif a=="paper":
             print("tie")
+            p=p+1
         else:
             print("you win")
-            p=p+1
-        print("point: ",p)
+            p=p+2
     else:
         if a=="rock":
             print("you win")
-            p=p+1
+            p=p+2
         elif a=="paper":
             print("you lose")
+            p=p-0.5
         else:
             print("tie")
-        print("point: ",p)
-    a= str(input("rock vs paper vs scissors: "))
+            p=p+1
+    print("point: ",p)
+    ran= random.randint(0,2)
+if p>=5:
+    print("You got all 5 points")
 
 
 
