4089cd3b1b72c7d4a7dda249e91030ab26c88522
Minjik06 <minjik525@deltalearns.ca>
2023-03-09 10:32:23 -0800

    random number

--- task2.py
+++ task2.py
@@ -3,20 +3,26 @@ Ask the player to choose heads or tails.
 Toss a coin to determine what the coin will be, and then tell the player if they were correct.
 (2 points)"""
 import random
-a= str(input("heads or tails?: "))
 frontC="heads"
 backC="tails"
+list=["heads", "tails"]
 ran= random.randint(0,1)
-if ran == 0:
-    ran = frontC
-    if ran == a:
-        print(ran, "you are correct")
+p=0
+while p<5:
+    ran= random.randint(0,1)
+    a= str(input("heads or tails?: "))
+    if ran == 0:
+        if a == list[ran]:
+            print(a, "you are correct")
+            p=p+1
+        else :
+            print(a, "you are wrong")
     else :
-        print(ran, "you are wrong")
-else :
-    ran = backC
-    if ran == a:
-        print(ran, "you are correct")
-    else :
-        print(ran, "you are wrong")
+        if a == list[ran]:
+            print(a, "you are correct")
+            p=p+1
+        else :
+            print(a, "you are wrong")
+    print(f"points: {p}")
+    
 
--- task4.py
+++ task4.py
@@ -12,30 +12,75 @@ Create a character generator that generates a character's statistics
 import random
 list = ["strength", "intelligence", "wisdom", "dexterity", "constitution", "charisma"]
 ran=random.randint(0,5)
-a=str("Enter the k to roll the dice")
+a=str(input("Enter the \'first\' to roll the dice: "))
 print("first dice")
 if ran == 0:
     k=list[0]
     print("first statistics recorded are: strength")
-    list.remove[0]
+    list.pop(0)
 elif ran == 1:
     print("first statistics recorded are: intelligence")
     k=list[1]
-    list.remove[1]
+    list.pop(1)
 elif ran == 2:
     print("first statistics recorded are: wisdom")
     k=list[2]
-    list.remove[2]
+    list.pop(2)
 elif ran == 3:
     print("first statistics recorded are: dexterity")
     k=list[3]
-    list.remove[3]
+    list.pop(3)
 elif ran == 4:
     print("first statistics recorded are: constitution")
     k=list[4]
-    list.remove[4]
+    list.pop(4)
 else :
     print("first statistics recorded are: charisma")
     k=list[5]
-    list.remove[5]
+    list.pop(5)
 
+b=str(input("Enter the \'second\' to roll second dice: "))
+print("second dice")
+ran=random.randint(0,4)
+if ran == 0:
+    j=list[0]
+    print(f"second statistics recorded are: {list[0]}")
+    list.pop(0)
+elif ran == 1:
+    print(f"second statistics recorded are: {list[1]}")
+    j=list[1]
+    list.pop(1)
+elif ran == 2:
+    print(f"second statistics recorded are: {list[2]}")
+    j=list[2]
+    list.pop(2)
+elif ran == 3:
+    print(f"second statistics recorded are: {list[3]}")
+    j=list[3]
+    list.pop(3)
+elif ran == 4:
+    print(f"second statistics recorded are: {list[4]}")
+    j=list[4]
+    list.pop(4)
+
+c=str(input("Enter the \'third\' to roll second dice: "))
+print("third dice")
+ran=random.randint(0,3)
+if ran == 0:
+    q=list[0]
+    print(f"third statistics recorded are: {list[0]}")
+    list.pop(0)
+elif ran == 1:
+    print(f"third statistics recorded are: {list[1]}")
+    q=list[1]
+    list.pop(1)
+elif ran == 2:
+    print(f"third statistics recorded are: {list[2]}")
+    q=list[2]
+    list.pop(2)
+elif ran == 3:
+    print(f"third statistics recorded are: {list[3]}")
+    q=list[3]
+    list.pop(3)
+
+print(f"Your characters three statistics are {k}, {j}, {q}")
